b2d01c829019cfde501e9d6f68a0aac9ebb16e02
ttran291 <ttran291@dxc.com>
2026-07-30 16:35:15 +0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![allow(unused_imports)]
 use std::io::{Read, Write};
 use std::net::TcpListener;
+use std::thread;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,17 +12,19 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                println!("Accept new connection");
+                thread::spawn(move || {
+                    println!("Accept new connection");
 
-                let mut buf = [0u8; 512];
-                loop {
-                    let bytes_read = stream.read(&mut buf).unwrap();
-                    if bytes_read == 0 {
-                        break; // client closed the connection (EOF)
+                    let mut buf = [0u8; 512];
+                    loop {
+                        let bytes_read = stream.read(&mut buf).unwrap();
+                        if bytes_read == 0 {
+                            break; // client closed the connection (EOF)
+                        }
+
+                        stream.write_all(b"+PONG\r\n").unwrap();
                     }
-
-                    stream.write_all(b"+PONG\r\n").unwrap();
-                }
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
