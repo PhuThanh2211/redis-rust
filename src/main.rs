d05ba53cab7be5fa4d4134aae48ebb40da99d406
ttran291 <ttran291@dxc.com>
2026-07-31 15:41:03 +0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,14 @@ use std::io::{Read, Write};
 use std::net::TcpListener;
 use std::sync::{Arc, Mutex};
 use std::thread;
+use std::time::{Duration, Instant};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Redis Server listening here!!!");
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    let store: Arc<Mutex<HashMap<String, String>>> = Arc::new(Mutex::new(HashMap::new()));
+    let store: Arc<Mutex<HashMap<String, (String, Option<Instant>)>>> = Arc::new(Mutex::new(HashMap::new()));
 
     for stream in listener.incoming() {
         match stream {
@@ -49,13 +50,31 @@ fn main() {
                             "SET" => {
                                 let key = args[1].to_string();
                                 let value = args[2].to_string();
-                                store.lock().unwrap().insert(key, value);
+
+                                let mut expiry: Option<Instant> = None;
+                                if args.len() >= 5 && args[3].to_uppercase() == "PX" {
+                                    let ms: u64 = args[4].parse().unwrap();
+                                    expiry = Some(Instant::now() + Duration::from_millis(ms));
+                                }
+
+                                store.lock().unwrap().insert(key, (value, expiry));
                                 stream.write_all(b"+OK\r\n").unwrap();
                             }
                             "GET" => {
                                 let key = args[1];
-                                let value = store.lock().unwrap().get(key).cloned();
-                                match value {
+                                let mut map = store.lock().unwrap();
+
+                                let result = match map.get(key) {
+                                    Some((value, Some(deadline)))
+                                    if Instant::now() >= *deadline => {
+                                        map.remove(key);        // lazily delete expired key
+                                        None
+                                    }
+                                    Some((value, _)) => Some(value.clone()),
+                                    None => None,
+                                };
+
+                                match result {
                                     Some(v) => {
                                         let response = format!("${}\r\n{}\r\n", v.len(), v);
                                         stream.write_all(response.as_bytes()).unwrap();
