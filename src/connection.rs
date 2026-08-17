569b8752eeb8fbbccad7313d9045cd19936e9cfb
ttran291 <ttran291@dxc.com>
2026-08-17 18:10:52 +0700

    codecrafters submit [skip ci]

--- src/connection.rs
+++ src/connection.rs
@@ -7,6 +7,7 @@ use crate::store::Store;
 
 struct ConnState {
     in_multi: bool,
+    queue: Vec<Vec<Vec<u8>>>,
 }
 
 pub fn handle(stream: TcpStream, store: Store) -> std::io::Result<()> {
@@ -14,7 +15,7 @@ pub fn handle(stream: TcpStream, store: Store) -> std::io::Result<()> {
     let mut writer = stream.try_clone()?;
     let mut reader = BufReader::new(stream);
 
-    let mut state = ConnState { in_multi: false };
+    let mut state = ConnState { in_multi: false, queue: Vec::new() };
 
     loop {
         match read_command(&mut reader)? {
@@ -40,7 +41,7 @@ fn handle_command(args: &[Vec<u8>], store: &Store, state: &mut ConnState) -> Res
         "MULTI" => {
             state.in_multi = true;
             Resp::Simple("OK".into())
-        },
+        }
         "EXEC" => {
             if !state.in_multi {
                 return Resp::Error("ERR EXEC without MULTI".into());
@@ -48,6 +49,11 @@ fn handle_command(args: &[Vec<u8>], store: &Store, state: &mut ConnState) -> Res
 
             state.in_multi = false;
             Resp::Array(vec![])
+        }
+        _ if state.in_multi => {
+            // queue the raw command; don't execute or touch the DB
+            state.queue.push(args.to_vec());
+            Resp::Simple("QUEUED".into())
         }
         // All non-transaction commands go to the stateless dispatcher.
         _ => dispatch(args, store),
