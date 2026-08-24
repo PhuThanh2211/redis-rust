19fc113c19daa070fd6c148643a34e7e23c5b782
ttran291 <ttran291@dxc.com>
2026-08-24 13:33:32 +0700

    codecrafters submit [skip ci]

--- src/replication.rs
+++ src/replication.rs
@@ -11,7 +11,6 @@ pub fn start_handshake(store: Store, my_port: u16) {
 
     std::thread::spawn(move || {
         let addr = format!("{host}:{port}");
-        println!("Redis Server listening here with port {port}!!!");
         match TcpStream::connect(&addr) {
             Ok(mut stream) => {
                 if let Err(e) = handshake(&mut stream, my_port) {
@@ -22,12 +21,18 @@ pub fn start_handshake(store: Store, my_port: u16) {
         }
     });
 
-    fn handshake(stream: &mut TcpStream, _my_port: u16) -> std::io::Result<()> {
+    fn handshake(stream: &mut TcpStream, my_port: u16) -> std::io::Result<()> {
         // Step 1: PING
         send_command(stream, &["PING"])?;
         let _ = read_reply(stream)?; // expect +PONG
 
         // Step 2: REPLCONF (twice)
+        let port_str = my_port.to_string();
+        send_command(stream, &["REPLCONF", "listening-port", &port_str])?;
+        let _ = read_reply(stream)?; // expect +OK
+
+        send_command(stream, &["REPLCONF", "capa", "psync2"])?;
+        let _ = read_reply(stream)?; // expect +OK
 
         // Step 3: PSYNC
 
